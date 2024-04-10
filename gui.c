5ef6d8d21b83cfe364b07ae496de1871103f53c8
CommanderSpinner <123424514+CommanderSpinner@users.noreply.github.com>
2024-04-10 21:08:16 +0200

    more update

--- gui.c
+++ gui.c
@@ -4,15 +4,27 @@ static void on_button_clicked(GtkWidget* button, gpointer user_data) {
     g_print("Button clicked!\n");
 }
 
-int main(int argc, char** argv) {
+void initGui(int argc, char** argv){
     gtk_init(&argc, &argv);
+    gtk_window_set_resizable(window, FALSE);
+    gtk_window_resize(GTK_WINDOW(window), 600, 400);
+}
 
-    GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    GtkWidget* button = gtk_button_new_with_label("Click me");
+
+int main(int argc, char** argv) {
+
+    initGui(&argc, &argv);
+
+    GtkWidget* button; 
+    button = gtk_button_new_with_label("Click me");
 
     g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
 
     gtk_container_add(GTK_CONTAINER(window), button);
+
+
+    // Größe des Fensters festlegen
+
     gtk_widget_show_all(window);
 
     gtk_main();
