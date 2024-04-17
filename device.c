e7e374ba67ad195fd06949bea49967214eb12442
CommanderSpinner <123424514+CommanderSpinner@users.noreply.github.com>
2024-04-17 19:11:40 +0200

    started to code with libusb

--- device.c
+++ device.c
@@ -1,5 +1,9 @@
 #include "device.h"
+
+/*
 void createDevice(Device *device, int id, const char* name){
     id = 0;
     device->name = name;
 }
+
+*/
--- device.h
+++ device.h
@@ -4,5 +4,6 @@ typedef struct{
     const char* name;
 }Device;
 
+Device* getDeviceNames();
 
-void createDevice(Device *device, int id, const char* name);
+//void createDevice(Device *device, int id, const char* name);
